f7b0ab7935650a92a1b9e6436d29eb479fee1d37
Sen Wang <wangsen992@gmail.com>
2020-05-04 22:40:24 +0800

    adjust_spines

--- src/cfog/utils.py
+++ src/cfog/utils.py
@@ -204,3 +204,23 @@ def fit_epsilon(Ek, k, alpha=1.7, k_range=(1,50)):
 
 def compute_kolmogov_spectrum(k, epsilon):
     return 1.7 * epsilon ** (2/3) * k ** (-5/3)
+
+def adjust_spines(ax,spines):
+    for loc, spine in ax.spines.items():
+        if loc in spines:
+            spine.set_position(('outward', 10))  # outward by 10 points
+        else:
+            spine.set_color('none') # don't draw spine 
+
+    # turn off ticks where there is no  spine
+    if 'left' in spines:
+        ax.yaxis.set_ticks_position('left')
+    else:
+        # no yaxis ticks
+        ax.yaxis.set_ticks([])
+
+    if 'bottom' in spines:
+        ax.xaxis.set_ticks_position('bottom')
+    else:
+        # no xaxis ticks
+        ax.xaxis.set_ticks([])
